4ecfc25a592c83cee4f7ddcf20eb9b5082fbcd0f
madhukardurgam <madhukar.durgam.8.7@gmail.com>
2025-03-19 00:11:43 +0530

    docker image

---
+++ docker.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Resize partition and extend volume
+growpart /dev/nvme0n1 4
+lvextend -l +50%FREE /dev/RootVG/rootVol
+lvextend -l +50%FREE /dev/RootVG/varVol
+xfs_growfs /
+xfs_growfs /var
+
+# Install Docker and dependencies
+dnf -y install dnf-plugins-core
+dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
+dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+
+# Start and enable Docker service
+systemctl start docker
+systemctl enable docker
+
+# Add ec2-user to the Docker group
+usermod -aG docker ec2-user
